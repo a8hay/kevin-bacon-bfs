34495764868afd246e7f4645aa9e821a7577309f
a8hay <46368902+a8hay@users.noreply.github.com>
2020-01-10 20:01:42 +0530

    added files via upload ..
    added queue and graph and data file

---
+++ graph.py
@@ -0,0 +1,94 @@
+import json
+from queue import Queue
+
+# use this for kavinbacon.json file
+with open("./kevinbacon.json") as json_file:
+	data = json.load(json_file)
+movies = data["movies"]
+
+
+class Vertex:
+	def __init__(self, key):
+		self.id = key
+		self.connectedTo = []
+		self.discovered = False
+		self.parent = None
+
+	def addNeighbor(self, other):
+		self.connectedTo.append(other)
+		other.connectedTo.append(self)
+
+	def __str__(self):
+		return str(self.id) + " connectedTo: " + str([x.id for x in self.connectedTo])
+
+
+class Graph:
+	def __init__(self):
+		self.vertList = {}
+		self.numVertices = 0
+
+	def addVertex(self, key):
+		self.numVertices+=1
+		newVertex = Vertex(key)
+		self.vertList[key] = newVertex
+		return newVertex
+
+	def addEdge(self, f, t):
+		if f not in self.vertList:
+			self.addVertex(f)
+		if t not in self.vertList:
+			self.addVertex(t)
+		self.vertList[f].addNeighbor(self.vertList[t])
+
+	def getVertex(self, item):
+		try:
+			return self.vertList[item]
+		except KeyError:
+			return None
+
+	def __iter__(self):
+		return iter(self.vertList.values())
+
+g = Graph()
+
+for movie in movies:
+	for actor in movie["cast"]:
+		g.addEdge(movie["title"].upper(), actor)
+
+
+def bfs():
+	s = input("enter start node")
+	e = input("enter end node")
+	start = g.getVertex(s)
+	end = g.getVertex(e)
+	if not start:
+		print(f"{s} not in graph")
+		return
+	if not end:
+		print(f"{e} not in graph")
+		return
+	Q = Queue()
+	start.discovered = True
+	Q.push(start)
+
+	while not Q.isEmpty():
+		current = Q.pop()
+		if current.id == end.id:
+			break
+
+		for neighbour in current.connectedTo:
+			if not neighbour.discovered:
+				neighbour.discovered = True
+				neighbour.parent = current
+				Q.push(neighbour)
+
+	curr = end
+	while curr.parent is not None:
+		if curr.id.isupper():
+			print(f"was in {curr.id} with")
+		else:
+			print(f"{curr.id}")
+		curr = curr.parent
+	print(start.id)
+
+bfs()
